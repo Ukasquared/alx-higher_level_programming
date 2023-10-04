a64eec64f0b2350efee3387a7e9f7fe4ca28fa12
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-05 00:48:20 +0100

    print last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,6 @@ print(f"Last digit of {number} is {new_number} and ", end="")
 if new_number > 5:
     print("is greater than 5")
 elif new_number < 6 and new_number != 0:
-    print("is less than 6 and not equal to 0")
+    print("is less than 6 and not 0")
 elif new_number == 0:
     print("is 0")

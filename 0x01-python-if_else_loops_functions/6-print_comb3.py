618e45b2312ea6287925ed32bc274dc57536cecd
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-05 03:57:08 +0100

    print ossibe combo of numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for num in  range(0, 9):
-    for num_2 in  range((num + 1), 10):
+for num in range(0, 9):
+    for num_2 in range((num + 1), 10):
         if num == 8 and num_2 == 9:
-            print("{}{}".format(num,num_2))
+            print("{}{}".format(num, num_2))
             break
         else:
             print("{}{}".format(num, num_2), end=', ')

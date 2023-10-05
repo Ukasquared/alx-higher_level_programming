adb8e7ed380016cb3fb7369e9fcff2c84bfa31ff
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-05 05:57:17 +0100

    updated executable

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def islower(c):
+    upperLimit = 97
+    lowerLimit = 122
+    if ord(c) <= lowerLimit:
+        if ord(c) >= upperLimit:
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    upperLimit = 122
+    loweLimit = 97
+    for str in str:
+        if ord(str) >= loweLimit and ord(str) <= upperLimit:
+            str = chr(ord(str) - 32)
+        print("{}".format(str), end="")
+    print("")

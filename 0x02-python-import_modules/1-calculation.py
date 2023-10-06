2378208af3c48fe66013530e2764c14e1fb8c1de
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-06 03:28:00 +0100

    use an imported func

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,10 +5,14 @@ from calculator_1 import add, sub, mul, div
 def f():
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} + {} = {}".format(a, b, sub(a, b)))
-    print("{} + {} = {}".format(a, b, mul(a, b)))
-    print("{} + {} = {}".format(a, b, div(a, b)))
+    add_res = add(a, b)
+    sub_res = sub(a, b)
+    mul_res = mul(a, b)
+    div_res = div(a, b)
+    print("{} + {} = {}".format(a, b, add_res))
+    print("{} + {} = {}".format(a, b, sub_res))
+    print("{} + {} = {}".format(a, b, mul_res))
+    print("{} + {} = {}".format(a, b, div_res))
 
 
 if __name__ == "__main__":

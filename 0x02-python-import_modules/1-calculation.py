79ee0b2df4ae4d46676ce57aa59162e38df30a42
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-06 03:32:29 +0100

    use an imported func

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -9,10 +9,10 @@ def f():
     sub_res = sub(a, b)
     mul_res = mul(a, b)
     div_res = div(a, b)
-    print("{} + {} = {}".format(a, b, add_res))
-    print("{} + {} = {}".format(a, b, sub_res))
-    print("{} + {} = {}".format(a, b, mul_res))
-    print("{} + {} = {}".format(a, b, div_res))
+    print(f"{} + {} = {}".format(a, b, add_res))
+    print(f"{} - {} = {}".format(a, b, sub_res))
+    print(f"{} * {} = {}".format(a, b, mul_res))
+    print(f"{} / {} = {}".format(a, b, div_res))
 
 
 if __name__ == "__main__":

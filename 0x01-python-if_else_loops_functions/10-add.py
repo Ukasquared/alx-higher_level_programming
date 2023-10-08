32c230534367939f22a88e7b8a8c5b068a2034c0
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-08 18:17:41 +0100

    add two numbers

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def add(a, b):
-    if type(a) is int and type(b) is int:
-        result = a + b
-        return result
-    else:
-        exit(1)
+    # if type(a) is int and type(b) is int:
+    result = a + b
+    return result

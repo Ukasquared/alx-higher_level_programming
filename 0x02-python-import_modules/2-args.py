34df7ba74aa0be359371f19c9d14d28c2a1978c1
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-11 16:29:14 +0100

    prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 import sys
 
-
-def cmdl():
+def cmd_line():
     """Print the number of and list of arguments."""
-    lenn = len(sys.argv) - 1
+    len_t = len(sys.argv) - 1
 
-    if lenn == 0:
-        print("0 arguments.")
-    elif lenn == 1:
+    if len_t == 0:
+        print("{} arguments.".format(len_t))
+    elif len_t == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(lenn))
+        print("{} arguments:".format(len_t))
     
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(i, arg))
-    if __name__ == "__main__":
-        cmdl()
+    for i, arg_m in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(i, arg_m))
+        
+if __name__ == "__main__":
+    cmd_line()

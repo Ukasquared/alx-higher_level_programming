02d9364cc93b391a4b6446e6a89e26824b79efed
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-11 05:47:21 +0100

    return list of number divisible by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+def divisible_by_2(my_list=[]):
+    new_List = []
+
+    if my_list:
+        for number in my_list:
+            if number % 2 == 0:
+                new_List.append(True)
+            else:
+                new_List.append(False)
+        return new_List

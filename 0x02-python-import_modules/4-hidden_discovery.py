8a5c2170d4da35020283e0575d17121f669d166b
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-12 00:13:08 +0100

    imported mod

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,10 +5,8 @@ if __name__ == "__main__":
     def name_s():
         # list of names in module
         names = dir(hidden_4)
-        #sort the list
-        names_sort = sorted(names)
-        for name in names_sort:
-            if name[:2] in names_sort == "__":
+        for name in names:
+            if name[:2] in names == "__":
                 continue
             else:
-                print("{}".format(names_sort))
+                print("{}".format(names))

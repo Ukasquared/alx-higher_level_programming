023ac2f36d41d8f3e86d585ef3520678ac1e90ad
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-25 19:35:57 +0100

    create instance of attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 # defining a square
 
+
+"""handling squares"""
+
+
 class Square:
-	"""instantiation of attribute"""
-	def __init__(self, size):
-		self.__size = size
+    """instantiation of attribute"""
+    def __init__(self, size):
+        self.__size = size

9a96bdc23bd23a3e77b3d843d2d8bb13cb1287d4
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-26 03:44:55 +0100

    defines a square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2
+# define a square module
+
+
+""" define a module that instatiates its attribute"""
+
+
+class Square:
+    """ handles a square values"""
+
+    def __init__(self, size=0):
+        """initializes an instance of the class
+        Args:
+	    self: represents the class
+	    size: size of square
+        """
+        if type(size) is int:
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")

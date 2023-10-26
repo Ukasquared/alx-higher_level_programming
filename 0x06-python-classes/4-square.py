077f21bce981cd16c3c3a69985e82cf27f0ca7bd
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-26 05:25:09 +0100

    enable access through setter method

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# define a square module
+
+
+""" define a module that instatiates its attribute"""
+
+
+class Square:
+    """ handles a square values"""
+
+    def __init__(self, size=0):
+        """initializes an instance of the class
+        Args:
+            self: represents the class
+            size: size of square
+        """
+        if type(size) is int:
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """returns size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size=0):
+        if type(size) is int:
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+
+    """defines the square area"""
+    def area(self):
+        """returns the arear"""
+        square = self.__size ** 2
+        return square

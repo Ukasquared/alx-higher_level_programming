72d18a3cd827680c598e46b41ee225cc99f2029c
Ukasquared <ukachukwujudith95@gmail.com>
2023-11-04 02:33:47 +0100

    divide a matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+This module divides all element of a matrix
+"""
+
+def matrix_divided(matrix, div):
+    """ This module divides integers or floats
+    Args:
+        matrix: list of list of integers and float
+        div: dividend
+    """
+    if matrix is None:
+        raise TypeError("matrix must be a matrix (list of lists) of integer/floats")
+    if div is None:
+        raise TypeError("div must be a number")
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integer/floats")
+    if not isinstance(div, (float, int)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    oldlen = len(matrix[0])
+    matrix_val = []
+    for i in range(len(matrix)):
+        matrix_val.append([])
+        if not oldlen == len(matrix[i]):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        for j in range(len(matrix[i])):
+            if not isinstance(matrix[i][j], (float, int)):
+                TypeError("matrix must be a matrix (list of lists) of integer/floats")
+            matrix_val[i].append(round(matrix[i][j] / div, 2))
+    return (matrix_val)

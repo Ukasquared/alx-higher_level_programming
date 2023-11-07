7c418a55f4db197cd162e57cdcf8f8d235f3b19e
Ukasquared <ukachukwujudith95@gmail.com>
2023-11-07 05:04:02 +0100

    return methond an attribut of object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""list of available attributes
+and methods of an object
+"""
+
+
+def lookup(obj):
+    """ returns the list of available
+    attributes and methods
+    Arg: obj
+    """
+    return dir(obj)
+

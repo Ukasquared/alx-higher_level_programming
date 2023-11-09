e5aa97e8dc0e8c248feeb247af695a01373a2bc4
Ukasquared <ukachukwujudith95@gmail.com>
2023-11-09 01:23:42 +0100

    working with inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,10 +5,9 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-
     """computes the size of a rectangle"""
     def __init__(self, width, height):
-        """ validates the integer """
+        """ instantiate self """
         super().integer_validator("width", width)
         super().integer_validator("height", height)
         self.__width = width
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -6,7 +6,6 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """computes the size of a rectangle"""
-
     def __init__(self, width, height):
         """ instance instantiation """
         super().integer_validator("width", width)

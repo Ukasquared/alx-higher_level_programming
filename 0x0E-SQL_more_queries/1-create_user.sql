a2602766e92dffa04b7d92058780e37d0362e6c3
Ukasquared <ukachukwujudith95@gmail.com>
2024-01-21 02:59:42 +0100

    grant all privilege to new user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creates a new user with password and grant all privilege
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES 
+ON *.* 
+TO 'user_0d_1'@'localhost';

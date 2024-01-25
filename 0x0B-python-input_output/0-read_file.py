11c4a4e4d114e648f7ea5b7ab9631c11d448a6e7
Ukasquared <ukachukwujudith95@gmail.com>
2024-01-25 19:25:13 +0100

    serialiazation and deserialization

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """ reads a file"""
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, 'r',  encoding="utf-8") as f:
         read_file = f.read()
     print(read_file)

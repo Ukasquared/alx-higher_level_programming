3f5600f849c05b70e0b62d89dec055c4e34c9b1c
Ukasquared <ukachukwujudith95@gmail.com>
2024-01-26 05:53:25 +0100

    save a list as json in a file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" student moduler"""
+import json
+
+
+class Student:
+    """ student class
+    """
+    def __init__(self, firstname, lastname, age):
+        """ instantiates the instance
+        with these attributes """
+        self.first_name = firstname
+        self.last_name = lastname
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ convert to dictionary
+        """
+        to_dict = self.__dict__
+        to_dict1 = {}
+        if attrs is not None:
+            for key, value in to_dict.items():
+                for attr in attrs:
+                    if attr == key:
+                        to_dict1[attr] = value
+            return to_dict1
+        return to_dict
+
+    def reload_from_json(self, json):
+        """ deserialization """
+        for key, values in json.items():
+            setattr(self, key, values)

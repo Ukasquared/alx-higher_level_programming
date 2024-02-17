f65404d608b8031328fe7024ed84a85303cc1335
Ukasquared <ukachukwujudith95@gmail.com>
2024-02-17 18:39:47 +0100

    filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" import modules """
+if __name__ == '__main__':
+  import sys
+  import MySQLdb
+
+  """ connect to existing database """
+  connection = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+  """ create cursor to interact with database""" 
+  cur = connection.cursor()
+  """ execute an sql query to retrieve names of state frm states table"""
+  query = "SELECT s.id, s.name FROM states s WHERE s.name = '{}' ORDER BY s.id ASC;"
+  cur.execute(query.format(sys.argv[4]))
+
+  for row in cur:
+    print(row)
+
+  cur.close
+  connection.close()

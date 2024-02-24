37bc3744480ef82612bd8f4c5bc64ad4540e04fd
Ukasquared <ukachukwujudith95@gmail.com>
2024-02-24 01:55:26 +0100

    filtering column

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 import sys
 from model_state import Base, State
@@ -7,13 +7,16 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    for result in session.query(State).order_by(State.id).filter(State.name == sys.argv[4]):
-        if result is None:
-            print("Not found")
-        else:
-            print(result.id)
+    query = session.query(State).order_by(State.id)\
+        .filter(State.name == sys.argv[4]).first()
+    if query is None:
+        print("Not found")
+    else:
+        print(query.id)

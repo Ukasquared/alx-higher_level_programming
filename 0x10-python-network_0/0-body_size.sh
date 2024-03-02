2924f8a804e075371780cb0f62c1b69ce833d95d
Ukasquared <ukachukwujudith95@gmail.com>
2024-03-02 03:05:42 +0100

    download size of body response

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# check if the argument is empty
+if [ -z "$1" ]; then
+	echo "Usage: $0 <URL>"
+	exit
+fi
+
+# if the first argument is true store the url
+url = "$1"
+
+# obtain the byte of the body of response in a variable
+response = $(curl -s -o dev/null -w "%{size_download}" "$url"}
+
+# print response if curl was succesful
+if [$? -ne 0 ]; then
+	echo "$response"
+fi

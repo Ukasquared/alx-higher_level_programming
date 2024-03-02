fb5e4b34bb7f87d3af1f1d656a7ac4d570d32d19
Ukasquared <ukachukwujudith95@gmail.com>
2024-03-02 13:41:52 +0100

    size of body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-# obtain the byte of the body of response in a variable
 response=$(curl -s -o dev/null -w "%{size_download}" "$1")
-
-# print response if curl was succesful
 echo "$response"

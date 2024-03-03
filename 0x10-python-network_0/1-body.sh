5089c7a42b6677d08e9d1c6c0e04e40ba2576b80
Ukasquared <ukachukwujudith95@gmail.com>
2024-03-02 16:16:40 -0800

    display response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the bodyresponse
-curl -s "$1"|grep "HTTP\/1.1 200" | sed -n "2p"
+curl -s "$1"|grep "HTTP\/1.1 200" | awk -v RS="\n" 'NR==2 {print}'

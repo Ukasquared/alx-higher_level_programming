d75493841f64dd06af2a0fcd19c2e5567b03414a
Ukasquared <ukachukwujudith95@gmail.com>
2024-03-05 16:43:12 +0100

    get header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,5 +7,5 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     response = requests.get(url)
-    content = response.headers['X-Request-Id']
+    content = response.headers.get('X-Request-Id')
     print(content)

723fedd1a0004025d88b8da145fd6a56e9f05513
Ukasquared <ukachukwujudith95@gmail.com>
2024-03-05 17:48:28 +0100

    print error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,11 +7,14 @@ from sys import argv
 if __name__ == "__main__":
     # gets the variables
     url = argv[1]
-    response = requests.get(url)
-    # get the status code
-    reponse_code = response.status_code
-    # check if response code is equal or greater than 400
-    if (response_code >= 400):
-        print(f"Error code: {response_code}")
-    else:
-        print(response.text)
+    try:
+        response = requests.get(url)
+        # get the status code
+        reponse_code = response.status_code
+        # check if response code is equal or greater than 400
+        if (response_code >= 400):
+            print(f"Error code: {response_code}")
+        else:
+            print(response.text)
+    except requests.ConnectionError as e:
+        print(e)

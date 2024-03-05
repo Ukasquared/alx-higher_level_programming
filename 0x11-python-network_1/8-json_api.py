7bb3e722aad79ee9dee0b026b7024a7273235e09
Ukasquared <ukachukwujudith95@gmail.com>
2024-03-05 18:43:39 +0100

    search api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -18,4 +18,4 @@ if __name__ == '__main__':
         else:
             print("[{}] {}".format(json_text['id'], json_text['name']))
     except ValueError:
-        print("not a JSON")
+        print("Not a valid JSON")

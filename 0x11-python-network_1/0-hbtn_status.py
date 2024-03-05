134d8124913e25bf77d8cd2d0d6a2f907c0d982a
Ukasquared <ukachukwujudith95@gmail.com>
2024-03-05 15:39:43 +0100

    fetch data from server

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,8 +8,8 @@ def request_response():
     with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         content = response.read()
         decode_content = content.decode('utf-8')
-        print(f'Body response:\n\t - type: {type(content)}\n\t - content:'
-            f' {content}\n\t - utf8 content: {decode_content}')
+        print(f'Body response:\n    - type: {type(content)}\n    - content:'
+            f' {content}\n    - utf8 content: {decode_content}')
 
 
 if __name__ == "__main__":

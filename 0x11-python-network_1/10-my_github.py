b55a264c1736449a0987948753aabd45b93b68f0
Ukasquared <ukachukwujudith95@gmail.com>
2024-03-06 03:10:05 +0100

    github API auth

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,6 +8,10 @@ if __name__ == "__main__":
     username = argv[1]
     PAT = argv[2]
     h_data = {"Authorization": f"Bearer {PAT}"}
+    # returns 
     response = requests.get(f"https://api.github.com/{username}",
                             headers=h_data)
-    print(response.headers.get('id'))
+    if (response.status_code >= 400):
+        print("None")
+    else:
+        print(response.json()["id"])

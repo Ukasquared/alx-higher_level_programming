7f55df61a3f4cf856779ccb11796718bc9c98f01
Ukasquared <ukachukwujudith95@gmail.com>
2024-03-06 03:22:09 +0100

    github API auth

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
     username = argv[1]
     PAT = argv[2]
     h_data = {"Authorization": f"Bearer {PAT}"}
-    # returns 
-    response = requests.get(f"https://api.github.com/{username}",
+    # returns object
+    link_param = f"https://api.github.com/{username}"
+    response = requests.get(link_param,
                             headers=h_data)
     if (response.status_code >= 400):
         print("None")
